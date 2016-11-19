2a7d7f54f20711720b08b71ee7cad7d5f62bd070
Lokesh Basu <lokesh@zerive.com>
2016-11-19 09:29:28 +0530

    feat: Initial commit

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+	}
+}
---
+++ Script Tutorial 1/Assets/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	private float fixedUpdateTimer;
+	private float updateTimer;
+
+	void FixedUpdate() {
+		Debug.Log ("FixedUpdate time: " + Time.deltaTime);
+	}
+
+	void Update() {
+		Debug.Log ("Update Time: "+Time.deltaTime);
+	}
+}
