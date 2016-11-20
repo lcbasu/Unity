5f5b1254248cb328c324dc6c1e2b647e4e25d583
Lokesh Basu <lokesh@zerive.com>
2016-11-20 12:12:45 +0530

    feat: Creates curve from series of points

--- Smartvizx Demo Project 2/Assets/Scripts/DrawCurve.cs
+++ Smartvizx Demo Project 2/Assets/Scripts/DrawCurve.cs
@@ -8,19 +8,14 @@ public class DrawCurve : MonoBehaviour {
 	private List<Vector3> pointsList;
 	private Vector3 mousePos;
 
-	// Structure for line points
-	struct myLine {
-		public Vector3 StartPoint;
-		public Vector3 EndPoint;
-	};
 	//	-----------------------------------	
 	void Awake() {
 		// Create line renderer component and set its property
 		line = gameObject.AddComponent<LineRenderer>();
 		line.material =  new Material(Shader.Find("Particles/Additive"));
 		line.SetVertexCount(0);
-		line.SetWidth(0.1f,0.1f);
-		line.SetColors(Color.green, Color.green);
+		line.SetWidth(.2f,0.2f);
+		line.SetColors(Color.red, Color.red);
 		line.useWorldSpace = true;	
 		isMousePressed = false;
 		pointsList = new List<Vector3>();
@@ -29,23 +24,24 @@ public class DrawCurve : MonoBehaviour {
 	//	-----------------------------------	
 	void Update () {
 		// If mouse button down, remove old line and set its color to green
-		if(Input.GetMouseButtonDown(0))
-		{
+		if(Input.GetMouseButtonDown(0)) {
 			isMousePressed = true;
 			line.SetVertexCount(0);
 			pointsList.RemoveRange(0,pointsList.Count);
-			line.SetColors(Color.green, Color.green);
+			line.SetColors(Color.red, Color.red);
 		}
-		else if(Input.GetMouseButtonUp(0))
-		{
+		else if(Input.GetMouseButtonUp(0)) {
 			isMousePressed = false;
+			for(int i = 0; i < pointsList.Count; i++)
+			{
+				Debug.Log("pointsList: " + pointsList[i]);
+			}
 		}
 		// Drawing line when mouse is moving(presses)
-		if(isMousePressed)
-		{
+		if(isMousePressed) {
 			mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			mousePos.z=0;
-			if (!pointsList.Contains (mousePos)) 			{
+			if (!pointsList.Contains (mousePos)) {
 				pointsList.Add (mousePos);
 				line.SetVertexCount (pointsList.Count);
 				line.SetPosition (pointsList.Count - 1, (Vector3)pointsList [pointsList.Count - 1]);
