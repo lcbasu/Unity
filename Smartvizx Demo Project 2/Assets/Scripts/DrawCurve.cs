6ee09ec246443fa5a430b8bb3736fb473bf4f031
Lokesh Basu <lokesh@zerive.com>
2016-11-20 12:43:56 +0530

    feat: Creates catcall spine from 4 points on curve

--- Smartvizx Demo Project 2/Assets/Scripts/DrawCurve.cs
+++ Smartvizx Demo Project 2/Assets/Scripts/DrawCurve.cs
@@ -8,6 +8,15 @@ public class DrawCurve : MonoBehaviour {
 	private List<Vector3> pointsList;
 	private Vector3 mousePos;
 
+	//Store points on the Catmull curve so we can visualize them
+	List<Vector3> newPoints = new List<Vector3>();
+
+	//How many points you want on the curve
+	float amountOfPoints = 10.0f;
+
+	//set from 0-1
+	public float alpha = 0.5f;
+
 	//	-----------------------------------	
 	void Awake() {
 		// Create line renderer component and set its property
@@ -36,6 +45,7 @@ public class DrawCurve : MonoBehaviour {
 			{
 				Debug.Log("pointsList: " + pointsList[i]);
 			}
+			CatmulRom();
 		}
 		// Drawing line when mouse is moving(presses)
 		if(isMousePressed) {
@@ -46,6 +56,55 @@ public class DrawCurve : MonoBehaviour {
 				line.SetVertexCount (pointsList.Count);
 				line.SetPosition (pointsList.Count - 1, (Vector3)pointsList [pointsList.Count - 1]);
 			}
+		}
+	}
+
+	void CatmulRom()
+	{
+		newPoints.Clear();
+
+		Vector3 p0 = new Vector3(pointsList[0].x, pointsList[0].y, 0.0f);
+		Vector3 p1 = new Vector3(pointsList[1].x, pointsList[1].y, 0.0f);
+		Vector3 p2 = new Vector3(pointsList[2].x, pointsList[2].y, 0.0f);
+		Vector3 p3 = new Vector3(pointsList[3].x, pointsList[3].y, 0.0f);
+
+		float t0 = 0.0f;
+		float t1 = GetT(t0, p0, p1);
+		float t2 = GetT(t1, p1, p2);
+		float t3 = GetT(t2, p2, p3);
+
+		for(float t=t1; t<t2; t+=((t2-t1)/amountOfPoints))
+		{
+			Vector3 A1 = (t1-t)/(t1-t0)*p0 + (t-t0)/(t1-t0)*p1;
+			Vector3 A2 = (t2-t)/(t2-t1)*p1 + (t-t1)/(t2-t1)*p2;
+			Vector3 A3 = (t3-t)/(t3-t2)*p2 + (t-t2)/(t3-t2)*p3;
+
+			Vector3 B1 = (t2-t)/(t2-t0)*A1 + (t-t0)/(t2-t0)*A2;
+			Vector3 B2 = (t3-t)/(t3-t1)*A2 + (t-t1)/(t3-t1)*A3;
+
+			Vector3 C = (t2-t)/(t2-t1)*B1 + (t-t1)/(t2-t1)*B2;
+
+			newPoints.Add(C);
+		}
+	}
+
+	float GetT(float t, Vector3 p0, Vector3 p1)
+	{
+		float a = Mathf.Pow((p1.x-p0.x), 2.0f) + Mathf.Pow((p1.y-p0.y), 2.0f) + Mathf.Pow((p1.z-p0.z), 2.0f);
+		float b = Mathf.Pow(a, 0.5f);
+		float c = Mathf.Pow(b, alpha);
+
+		return (c + t);
+	}
+
+	//Visualize the points
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		foreach(Vector3 temp in newPoints)
+		{
+			Vector3 pos = new Vector3(temp.x, temp.y, 0);
+			Gizmos.DrawSphere(pos, 0.3f);
 		}
 	}
 }
