40792aa1cf280af5a3e043fe6a2466aac1687022
Lokesh Basu <lokesh@zerive.com>
2016-11-21 12:39:10 +0530

    feat: Creates catmull spline 😀

--- Smartvizx Demo Project 2/Assets/Scripts/DrawCurve.cs
+++ Smartvizx Demo Project 2/Assets/Scripts/DrawCurve.cs
@@ -13,6 +13,11 @@ public class DrawCurve : MonoBehaviour {
 	//Store points on the Catmull curve so we can visualize them
 	List<Vector3> newPoints = new List<Vector3>();
 
+	//Store all points on the Catmull curve
+	List<Vector3> allSplinePoints = new List<Vector3>();
+
+	private LineRenderer catmullSpline;
+
 	//set from 0-1
 	public float alpha = 0.5f;
 
@@ -27,7 +32,6 @@ public class DrawCurve : MonoBehaviour {
 		line.useWorldSpace = true;	
 		isMousePressed = false;
 		pointsList = new List<Vector3>();
-		//		renderer.material.SetTextureOffset(
 	}
 	//	-----------------------------------	
 	void Update () {
@@ -35,17 +39,31 @@ public class DrawCurve : MonoBehaviour {
 		if(Input.GetMouseButtonDown(0)) {
 			isMousePressed = true;
 			line.SetVertexCount(0);
-			pointsList.RemoveRange(0,pointsList.Count);
+			pointsList.RemoveRange(0, pointsList.Count);
+			allSplinePoints.RemoveRange(0, allSplinePoints.Count);
 			line.SetColors(Color.red, Color.red);
 		}
 		else if(Input.GetMouseButtonUp(0)) {
 			isMousePressed = false;
-			Debug.Log ("Total number of points: " + pointsList.Count);
-			for(int i = 0; i < pointsList.Count; i++)
-			{
-				Debug.Log("pointsList: " + pointsList[i]);
+			Debug.Log ("Total number of mouse points: " + pointsList.Count);
+//			CatmulRom();
+			pointsList.Add (pointsList[0]);
+			pointsList.Add (pointsList[1]);
+			pointsList.Add (pointsList[2]);
+			Debug.Log ("[NEW]Total number of mouse points: " + pointsList.Count);
+
+			// Drawing Catmull curve between all the points
+			for(int i = 0; i < pointsList.Count - 3; i++) { // only  run the loop till (count - 4) element 
+				addPointsOnCatmullCurve(pointsList[i], pointsList[i+1], pointsList[i+2], pointsList[i+3]);
 			}
-			CatmulRom();
+			Debug.Log ("Total number of spline points: " + allSplinePoints.Count);
+			allSplinePoints.Add (pointsList[pointsList.Count - 2]);
+			Debug.Log ("[NEW]Total number of spline points: " + allSplinePoints.Count);
+
+			line.SetVertexCount (allSplinePoints.Count-1);
+			for(int i = 0; i < allSplinePoints.Count - 1; i++) {
+				line.SetPosition (i, allSplinePoints[i]);
+			}
 		}
 		// Drawing line when mouse is moving(presses)
 		// The points are pushed each frame
@@ -57,6 +75,35 @@ public class DrawCurve : MonoBehaviour {
 				line.SetVertexCount (pointsList.Count);
 				line.SetPosition (pointsList.Count - 1, (Vector3)pointsList [pointsList.Count - 1]);
 			}
+		}
+	}
+
+	void drawCatmullSpline() {
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.SetPositions(allSplinePoints.ToArray());
+	}
+
+	void addPointsOnCatmullCurve(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) {
+
+		float t0 = 0.0f;
+		float t1 = GetT(t0, p0, p1);
+		float t2 = GetT(t1, p1, p2);
+		float t3 = GetT(t2, p2, p3);
+
+		float amountOfPoints = numberOfPointsBetweenSplinePoints;
+
+		for(float t=t1; t<t2; t+=((t2-t1)/amountOfPoints))
+		{
+			Vector3 A1 = (t1-t)/(t1-t0)*p0 + (t-t0)/(t1-t0)*p1;
+			Vector3 A2 = (t2-t)/(t2-t1)*p1 + (t-t1)/(t2-t1)*p2;
+			Vector3 A3 = (t3-t)/(t3-t2)*p2 + (t-t2)/(t3-t2)*p3;
+
+			Vector3 B1 = (t2-t)/(t2-t0)*A1 + (t-t0)/(t2-t0)*A2;
+			Vector3 B2 = (t3-t)/(t3-t1)*A2 + (t-t1)/(t3-t1)*A3;
+
+			Vector3 C = (t2-t)/(t2-t1)*B1 + (t-t1)/(t2-t1)*B2;
+
+			allSplinePoints.Add(C);
 		}
 	}
 
@@ -100,11 +147,11 @@ public class DrawCurve : MonoBehaviour {
 		return (c + t);
 	}
 
-	//Visualize the points
+	//Visualize the catmull spline points
 	void OnDrawGizmos()
 	{
 		Gizmos.color = Color.red;
-		foreach(Vector3 temp in newPoints)
+		foreach(Vector3 temp in allSplinePoints)
 		{
 			Vector3 pos = new Vector3(temp.x, temp.y, 0);
 			Gizmos.DrawSphere(pos, 0.3f);
