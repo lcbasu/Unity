9db26c3edfe10535ad6ae466893f351b17f36249
Lokesh Basu <lokesh.basu@gmail.com>
2016-12-06 09:29:28 +0530

    fix: Deletes old project

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PlayerController : MonoBehaviour {
-	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-	}
-}
