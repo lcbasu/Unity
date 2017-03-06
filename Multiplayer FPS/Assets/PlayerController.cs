6d9587ba7d4c15053840b68333693806c5a7e935
Lokesh Basu <lokesh.basu@gmail.com>
2017-03-06 11:29:32 +0530

    feat: Adds movement to the player

---
+++ Multiplayer FPS/Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // Calculate movement velovity as 3D Vector
+
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMovement;
+        Vector3 moveVertical = transform.forward * zMovement;
+
+        // Final movement vector
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        // Apply movement
+
+        motor.Move(velocity);
+
+        
+    }
+}
---
+++ Multiplayer FPS/Assets/PlayerMotor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Gets a movement vector
+    public void Move(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    // Runs every physics iteration
+    void FixedUpdate()
+    {
+        PerormMovement();
+    }
+
+    // Perform movement based on velocity variable
+    private void PerormMovement()
+    {
+        if (this.velocity != Vector3.zero)
+        {
+            rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+}
