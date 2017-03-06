d0ce29427cf59f2a8b99e4fc43dd5a75771d7570
Lokesh Basu <lokesh.basu@gmail.com>
2017-03-06 14:13:01 +0530

    feat: Adds network manager

---
+++ Multiplayer FPS/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    public List<Behaviour> componentsToDisable = null;
+
+    Camera sceneCamera = null;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (Behaviour b in componentsToDisable)
+            {
+                b.enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+}
