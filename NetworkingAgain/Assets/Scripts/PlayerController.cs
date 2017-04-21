69673725bde3de0edc8f6daf3edcae57ec7394f0
Lokesh Basu <lokesh.basu@gmail.com>
2017-04-21 10:07:03 +0530

    feat: Adds color to local player to distinguish

--- NetworkingAgain/Assets/Scripts/PlayerController.cs
+++ NetworkingAgain/Assets/Scripts/PlayerController.cs
@@ -15,4 +15,11 @@ public class PlayerController : NetworkBehaviour
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
     }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
 }
